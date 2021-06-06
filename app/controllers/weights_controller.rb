e1393fc9a95b90bdd88558a47e2f8be50b23073b
nisitin <seiya110307@icloud.com>
2021-06-06 13:15:31 +0900

    feat: 新機能

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -1,2 +1,30 @@
 class WeightsController < ApplicationController
+  def index
+    # @weights = Weight.all
+  end
+
+  def new
+  end
+
+  def show
+    @weight = find([:id])
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  private
+
+  def weight_params
+  end
+
 end
