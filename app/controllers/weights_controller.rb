0595740970a5778a9f577fea9db42cdebbbebf97
nisitin <seiya110307@icloud.com>
2021-06-09 18:57:10 +0900

    feat: 今日の更新分

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -1,9 +1,12 @@
 class WeightsController < ApplicationController
   def index
-    # @weights = Weight.all
+    @weights = Weight.all
+    # ↓ロジック的にはログインしたユーザが持っている体重という感じ
+    # @weight_chart = current_user.weights.value
   end
 
   def new
+    @weights = Weight.new
   end
 
   def show
@@ -11,6 +14,13 @@ class WeightsController < ApplicationController
   end
 
   def create
+    @weight = current_user.weight.build(weight_params)
+
+    if @weight.save
+       redirect_to root_path, flash: { notice: '体重の投稿が反映されました' }
+    else
+       render :new
+    end
   end
 
   def edit
@@ -19,12 +29,10 @@ class WeightsController < ApplicationController
   def update
   end
 
-  def create
-  end
-
   private
 
   def weight_params
+    params.require(:weight).permit(:value)
   end
 
 end
