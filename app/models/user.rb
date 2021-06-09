ab46d8303586c28c6236b773e4823314fb60a51e
nisitin <seiya110307@icloud.com>
2021-06-09 11:58:23 +0900

    fix: dependentの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :weight
-  has_many :profile
+  # これは親要素が消絵たらこ要素も消えると言う意味
+  has_many :weight, dependent: :destroy
+  has_many :profile, dependent: :destroy
 end
