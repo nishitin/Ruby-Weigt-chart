46424b0c87bd5a5afe59c84c0d6eeef6947777fa
nisitin <seiya110307@icloud.com>
2021-06-15 15:13:40 +0900

    feat: rspecの設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :user do
-    name { 'TestName'}
     sequence(:email) { |n| 'test#{n}@gmail.com' }
     password { 'password' }
     password_confirmation { 'password' }
