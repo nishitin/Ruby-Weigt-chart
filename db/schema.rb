68869538ab4894992938aa418662675a2373cee1
nisitin <seiya110307@icloud.com>
2021-06-15 14:12:29 +0900

    fix: dbの値の修正

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_06_04_003141) do
   create_table "weights", force: :cascade do |t|
     t.integer "user_id", null: false
     t.float "value", default: 0.0, null: false
+    t.time "input_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_weights_on_user_id"
