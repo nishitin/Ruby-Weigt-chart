851b8171540a85e58c7e023205fef7db16f60b36
nisitin <seiya110307@icloud.com>
2021-06-16 12:45:28 +0900

    feat: weight_specの導入

--- spec/requests/weights_spec.rb
+++ spec/requests/weights_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "Weights", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Weights', type: :request do
+
+  before { sign_in user }
+
+  let(:user) { create(:user) }
+
+  describe 'GET index' do
+    before { get weights_path }
+
+    it 'statusコードが200であること' do
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe 'GET new' do
+    before { get new_weight_path }
+
   end
 end
